eef789f016f63b31fa57dd112650571cd2b8247f
YoungWan Kweon <87213416+000wan@users.noreply.github.com>
2023-05-19 20:47:31 +0900

    Add horizontal banner

--- client/src/components/ZaboAds.jsx
+++ client/src/components/ZaboAds.jsx
@@ -5,15 +5,16 @@ import './ZaboAds.css'
 const ZaboAds = () => {
   const [ zaboData, setZaboData ] = useState(
     {
-      "poster": "https://sparcs-kaist-zabo-prod.s3.ap-northeast-2.amazonaws.com/zabo/zabo-136421683085229471",
-      "background_color": "#643671",
-      "text_color": "#ffffff",
-      "author": "SPARCS",
-      "author_img": "https://sparcs-kaist-zabo-prod.s3.ap-northeast-2.amazonaws.com/profile/group-1583234938504",
-      "title": "Taxi: Kaist를 위한 택시 승차 공유 서비스",
-      "description": "택시 탑승 인증 이벤트 및 인스타그램 공유 이벤트 진행 중!"
+      poster: "",
+      background_color: "",
+      text_color: "",
+      author: "",
+      author_img: "",
+      title: "",
+      description: ""
     }
   )
+  const [ isHorizontal, setIsHorizontal ] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,30 +24,49 @@ const ZaboAds = () => {
     fetchData();
   }, [])
 
+  useEffect(() => {
+    const img = new Image()
+    img.src = zaboData.poster
+    img.onload = () => {
+      if (img.width >= img.height) {
+        setIsHorizontal(true)
+      } else {
+        setIsHorizontal(false)
+      }
+    }
+  }, [ zaboData ])
+
   return (
     <div className="zabo-ads">
       <div
         className="zabo-ads__container"
         style={{
           backgroundColor: zaboData.background_color,
-          color: zaboData.text_color
+          color: zaboData.text_color,
+          borderColor: zaboData.text_color
         }}
       >
         <div className="zabo-ads__content">
           <img
             className="zabo-ads__content__image"
             src={ zaboData.poster }
+            style={
+              isHorizontal ?
+              { filter: "blur(3px)" } :
+              {}
+            }
             alt="Zabo Poster"
           />
-          <div className="zabo-ads__content__title">
-            <h3>
-              { zaboData.title }
-            </h3>
-          </div>
-          <div className="zabo-ads__content__description">
-            <p>
-              { zaboData.description }
-            </p>
+          <div className={isHorizontal ?
+            "zabo-ads__content__text--horizontal" :
+            "zabo-ads__content__text"
+          }>
+            <div className="zabo-ads__content__title">
+              <h3> { zaboData.title } </h3>
+            </div>
+            <div className="zabo-ads__content__description">
+              <p> { zaboData.description } </p>
+            </div>
           </div>
         </div>
       </div>
